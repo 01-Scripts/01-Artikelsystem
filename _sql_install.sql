86cb1831041491225ed9746fde415929753db2b4
01-Scripts <info@01-scripts.de>
2011-09-04 18:14:37 +0200

    #297 Neue Spalte auch in _sql_install.sql mit eingefügt

--- _sql_install.sql
+++ _sql_install.sql
@@ -119,6 +119,7 @@ CREATE TABLE `01modulprefix_article` (
   `top` tinyint(1),
   `hits` int(10) default '0',
   `hide_signature` tinyint(1) default '0',
+  `serialized_data` mediumblob COMMENT 'use unserialize() to get data back',
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=2 ;
 
